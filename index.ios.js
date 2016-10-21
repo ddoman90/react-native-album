87837434c0bb0a97a1cbb1860a093abbca6bd409
ddoman90 <ddoman90@gmail.com>
2016-10-21 18:49:37 +0200

    until Section 6 - Making great looking apps

---
+++ index.ios.js
@@ -0,0 +1,16 @@
+// Place code in here for IOS
+
+
+// Import a library to help create a component
+import React from 'react';
+import { AppRegistry } from 'react-native';
+
+import Header from './src/components/header'
+
+// Create a component
+const App = () => (
+    <Header headerText={'Albums'} />
+);
+
+// Render it to the device
+AppRegistry.registerComponent('albums', () => App);
---
+++ readme.md
@@ -0,0 +1,9 @@
+React:
+- knows how a component should behave
+- knows how to take a bunch of components and make them work together
+
+React Native:
+- knows how to take the output from a component and place it on the screen
+- provides default core components
+
+JSX is only a syntactic sugar it compiles to normal JS
